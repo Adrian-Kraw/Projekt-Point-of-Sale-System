8f430b98468ce379fdba35b73a08f47994c62dff
Adrian Krawietz <krawietz.adriandavid@fh-swf.de>
2026-03-27 15:25:28 +0100

    Grundeinrichtung 1

---
+++ src/main/java/de/fhswf/kassensystem/KassensystemApplication.java
@@ -0,0 +1,13 @@
+package de.fhswf.kassensystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KassensystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KassensystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fhswf/kassensystem/KassensystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fhswf.kassensystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KassensystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
