8bf4ebd3dabb96c5d5f035966d4bdefc05b0561a
Paula Martin <martin.paula@fh-swf.de>
2026-04-02 14:49:22 +0200

    UserService hinzugefügt

--- src/main/java/de/fhswf/kassensystem/repository/UserRepository.java
+++ src/main/java/de/fhswf/kassensystem/repository/UserRepository.java
@@ -9,5 +9,11 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // ...
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    User getUserById(Long id);
 }
