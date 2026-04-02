400ce876ebb1bc486ed6384807e404436a878ead
Paula Martin <martin.paula@fh-swf.de>
2026-04-02 13:09:34 +0200

    ArtikelService hinzugefügt

--- src/test/java/de/fhswf/kassensystem/model/enums/Mehrwertsteuer.java
+++
@@ -1,4 +0,0 @@
-package de.fhswf.kassensystem.model.enums;
-
-public enum Mehrwertsteuer {
-}
--- src/test/java/de/fhswf/kassensystem/model/enums/Zahlungsart.java
+++
@@ -1,9 +0,0 @@
-package de.fhswf.kassensystem.model.enums;
-
-/**
- * Definiert die verfügbaren Zahlungsarten im Kassensystem.
- */
-public enum Zahlungsart {
-    BAR,
-    KARTE
-}
